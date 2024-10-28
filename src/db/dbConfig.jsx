2ef7f71979975321b150bded607f2bdc0da8ee04
Engraya <ahmadyakubuaya@gmail.com>
2024-10-28 06:26:07 +0100

    Updated Config file and .env

--- src/db/dbConfig.jsx
+++ src/db/dbConfig.jsx
@@ -1,10 +1,11 @@
+"use server";
 
 import { neon } from "@neondatabase/serverless";
-
 import { drizzle } from "drizzle-orm/neon-http";
-
 import * as schema from "./schema";
 
 const sql = neon(process.env.DATABASE_URL);
 
-export const db = drizzle(sql, { schema });
+export async function getDb() {
+  return drizzle(sql, { schema });
+}
