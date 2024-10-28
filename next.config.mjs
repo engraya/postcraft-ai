067b2f979fc23cb708849f1735a99a9496576138
Engraya <ahmadyakubuaya@gmail.com>
2024-10-28 06:46:17 +0100

    Fixed

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ["cdn.sanity.io", "i.imgur.com"],
+      },
+};
+
+export default nextConfig;
--- src/db/dbConfig.jsx
+++ src/db/dbConfig.jsx
@@ -1,11 +1,11 @@
-"use server";
+"use server"
 
 import { neon } from "@neondatabase/serverless";
+
 import { drizzle } from "drizzle-orm/neon-http";
+
 import * as schema from "./schema";
 
 const sql = neon(process.env.DATABASE_URL);
 
-export async function getDb() {
-  return drizzle(sql, { schema });
-}
+export const db = drizzle(sql, { schema });
